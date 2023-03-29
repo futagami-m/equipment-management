f0a969a84685a162a994d6f8418d599ae76b179d
futagami-m <hana20190406@gmail.com>
2023-03-29 15:02:37 +0900

    https通信にする

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\App; // 追加
+use Illuminate\Support\Facades\URL; // 追加
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,8 +27,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-        if(\App::environment(['production'])){
-            \URL::forceScheme('https');
+        if(App::environment(['production'])){
+            URL::forceScheme('https');
         }
     }
 }
