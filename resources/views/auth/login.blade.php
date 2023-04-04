87838dff34b59e5e2d0e137c3e9cfaa11d185384
futagami-m <hana20190406@gmail.com>
2023-04-04 14:21:46 +0900

    ログイン画面更新

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -58,11 +58,7 @@
                                     {{ __('Login') }}
                                 </button>
 
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Passwordを忘れましたか?') }}
-                                    </a>
-                                @endif
+                               
                             </div>
                         </div>
                     </form>
